a2cee982ddd22fbb510e1a6cc53ae85e58b9d042
Eric Shi <ericshi98@yahoo.com>
2020-03-30 16:23:59 -0400

    Added Heroku mod

---
+++ static/pcrud-xhr.js
@@ -0,0 +1,58 @@
+const url = "https://serene-escarpment-90518.herokuapp.com/";
+
+function counterCreate() {
+    console.log('trigger');
+    (async () => {
+        let counterName = document.getElementById("countername").value;
+        const newURL = url + "/create?name=" + counterName;  // this code will change
+        const resp = await fetch(newURL);
+        const j = await resp.json();
+        if (j['result'] !== 'error') {
+            document.getElementById("output").innerHTML = "101: <b>" + counterName + " created with value " + j['value'] + "</b>";  // this code will change
+        } else {
+            document.getElementById("output").innerHTML = "100: " + counterName + " not found.</b>";
+        }
+    })();
+}
+
+function counterRead() {
+    (async () => {
+        let counterName = document.getElementById("countername").value;
+        const newURL = url + "/read?name=" + counterName;  // this code will change
+        const resp = await fetch(newURL);
+        const j = await resp.json();
+        if (j['result'] !== 'error') {
+            document.getElementById("output").innerHTML = "201: <b>" + counterName + " has value " + j['value'] + "</b>";  // this code will change
+        } else {
+            document.getElementById("output").innerHTML = "200: " + counterName + " not found.</b>";
+        }
+    })();
+}
+
+function counterUpdate() {
+    (async () => {
+        let counterName = document.getElementById("countername").value;
+        const newURL = url + "/update?name=" + counterName;  // this code will change
+        const resp = await fetch(newURL);
+        const j = await resp.json();
+        if (j['result'] !== 'error') {
+            document.getElementById("output").innerHTML = "301: <b>Updated " + counterName + " with value " + j['value'] + "</b>";  // this code will change
+        } else {
+            document.getElementById("output").innerHTML = "300: " + counterName + " not found.</b>";
+        }
+    })();
+}
+
+function counterDelete() {
+    (async () => {
+        let counterName = document.getElementById("countername").value;
+        const newURL = url + "/delete?name=" + counterName;  // this code will change
+        const resp = await fetch(newURL);
+        const j = await resp.json();
+        if (j['result'] !== 'error') {
+            document.getElementById("output").innerHTML = "401: <b>Deleted " + counterName + "</b>";  // this code will change
+        } else {
+            document.getElementById("output").innerHTML = "400: " + counterName + " not found.</b>";
+        }
+    })();
+}
